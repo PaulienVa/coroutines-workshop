113fea452b48b3a76956708e0cbb1fef5a52f4c8
Paulien van Alst <coding@paulienvanalst.com>
2021-01-08 11:04:19 +0100

    Skeleton for workshop runners

---
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/coroutines/workshop/runners/WorkshopRunner.kt
@@ -0,0 +1,17 @@
+package nl.openvalue.paulienvanalst.kotlin.coroutines.workshop.runners
+
+object WorkshopRunner {
+
+    fun run() {
+        println("Which exercise do you want to execute? (1/2/3)")
+        val exNr = readLine()
+
+        when(exNr){
+            "1" -> Exercise1Runner.run()
+            "2" -> Exercise2Runner.run()
+            "3" -> Exercise3Runner.run()
+            else -> println("No valid exercise... exiting ...")
+        }
+
+    }
+}
