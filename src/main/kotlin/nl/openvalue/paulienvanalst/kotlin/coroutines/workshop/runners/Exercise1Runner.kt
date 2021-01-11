9d73344c294f54747c5e6d62479f8ee0320be0f0
Paulien van Alst <coding@paulienvanalst.com>
2021-01-11 11:25:04 +0100

    Remove timeout block (not suitable for final solution)

--- src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/coroutines/workshop/runners/Exercise1Runner.kt
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/coroutines/workshop/runners/Exercise1Runner.kt
@@ -12,14 +12,12 @@ object Exercise1Runner {
     fun run() {
         printlnCW("[RUNNER]: Running Exercise 1...\n")
         runBlocking {
-            withTimeout(10){
-                val waterPan = async { Cooker.boil(Pan(1, Liquid.WATER, 10)) }
-                val oliveOilPan = async { Cooker.boil(Pan(1, Liquid.OLIVE_OIL, 10)) }
+            val waterPan = async { Cooker.boil(Pan(1, Liquid.WATER, 10)) }
+            val oliveOilPan = async { Cooker.boil(Pan(1, Liquid.OLIVE_OIL, 10)) }
 
-                if (waterPan.await() && oliveOilPan.await()) {
-                    println("\n")
-                    printlnCW("[RUNNER]: Both pans are finished boiling \n")
-                }
+            if (waterPan.await() && oliveOilPan.await()) {
+                println("\n")
+                printlnCW("[RUNNER]: Both pans are finished boiling \n")
             }
             printlnCW("[RUNNER]: Finishing exercise 1\n")
         }
